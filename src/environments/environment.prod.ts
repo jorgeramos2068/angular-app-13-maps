4ec5cdbaf8ab07888b93a496805823055a1929b0
Jorge Ramos <jose.ramos@nextiva.com>
2021-10-28 10:16:57 -0500

    Install and configure Mapbox

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken:
+    'pk.eyJ1Ijoiam9zZXJhbW9zdHciLCJhIjoiY2t2YjFzczNvOThtZDJ2dDl5Z3l2cmt6YiJ9.tWzy7BqzCyeIlvsluX7ZyA',
 };
