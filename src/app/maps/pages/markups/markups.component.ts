c9f47b2b1e8ecdeb6fcaef42b5d35d32c5e0be5e
Jorge Ramos <jose.ramos@nextiva.com>
2021-10-28 09:58:31 -0500

    Create initial components and routes

---
+++ src/app/maps/pages/markups/markups.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-markups',
+  templateUrl: './markups.component.html',
+  styles: [
+  ]
+})
+export class MarkupsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
