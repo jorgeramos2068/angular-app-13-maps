bb9563b56dc40e59f8e991ca7bddcdad1f530b88
Jorge Ramos <jose.ramos@nextiva.com>
2021-11-03 09:09:17 -0600

    Add functionality to keep markers arrays with colors

--- src/app/maps/pages/markers/markers.component.ts
+++ src/app/maps/pages/markers/markers.component.ts
@@ -1,6 +1,11 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
+interface CustomMarker {
+  color: string;
+  marker: mapboxgl.Marker;
+}
+
 @Component({
   selector: 'app-markers',
   templateUrl: './markers.component.html',
@@ -10,6 +15,7 @@ export class MarkersComponent implements AfterViewInit {
   public map!: mapboxgl.Map;
   public zoomLevel: number = 15;
   public mapCenter: [number, number] = [-103.38991542336164, 20.65440818024887];
+  public markers: CustomMarker[] = [];
 
   constructor() {}
 
@@ -28,12 +34,16 @@ export class MarkersComponent implements AfterViewInit {
     const color: string = '#xxxxxx'.replace(/x/g, (y) =>
       ((Math.random() * 16) | 0).toString(16)
     );
-    const newMarket: mapboxgl.Marker = new mapboxgl.Marker({
+    const newMarker: mapboxgl.Marker = new mapboxgl.Marker({
       draggable: true,
       color,
     })
       .setLngLat(this.mapCenter)
       .addTo(this.map);
+    this.markers.push({
+      color,
+      marker: newMarker,
+    });
   }
 
   goToMarker(): void {
