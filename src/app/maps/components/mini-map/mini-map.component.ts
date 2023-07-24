d2e666f43a91e910b6a90dcde8f8c324600727eb
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-24 14:58:31 -0600

    Implement mini map component

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-map',
   templateUrl: './mini-map.component.html',
-  styles: [
-  ]
 })
-export class MiniMapComponent implements OnInit {
+export class MiniMapComponent implements AfterViewInit {
+  @Input() lngLat: [number, number] = [0, 0];
+  @ViewChild('map') mapDiv!: ElementRef;
+  public map!: mapboxgl.Map;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+      center: this.lngLat,
+      container: this.mapDiv.nativeElement,
+      interactive: false,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      zoom: 15,
+    });
+
+    new mapboxgl.Marker().setLngLat(this.lngLat).addTo(this.map);
   }
-
 }
--- src/app/maps/pages/estates/estates.component.ts
+++ src/app/maps/pages/estates/estates.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Estate {
+  title: string;
+  description: string;
+  lngLat: [number, number];
+}
 
 @Component({
   selector: 'app-estates',
   templateUrl: './estates.component.html',
-  styles: [
-  ]
 })
-export class EstatesComponent implements OnInit {
+export class EstatesComponent {
+  public estates: Estate[] = [
+    {
+      title: 'Casa residencial, Canadá',
+      description: 'Bella propiedad en Katana, Canadá',
+      lngLat: [-75.92722289474008, 45.280015511264466],
+    },
+    {
+      title: 'Casa de playa, México',
+      description: 'Hermosa casa de playa en Acapulco, México',
+      lngLat: [-99.91287720907991, 16.828940930185748],
+    },
+    {
+      title: 'Apartamento, Argentina',
+      description:
+        'Lujoso apartamento en el corazón de Buenos Aires, Argentina',
+      lngLat: [-58.430166677283445, -34.57150108832866],
+    },
+    {
+      title: 'Local comercial, España',
+      description:
+        'Local comercial disponible en Madrid, España, cerca de El Jardín Secreto.',
+      lngLat: [-3.7112735618380177, 40.42567285425766],
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
 }
