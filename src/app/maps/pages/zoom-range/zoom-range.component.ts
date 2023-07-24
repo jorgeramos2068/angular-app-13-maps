ef0c996e471cd1a780c8531da93bc5964cdaa5cf
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-24 14:58:24 -0600

    Implement zoom functionality

--- src/app/maps/pages/zoom-range/zoom-range.component.ts
+++ src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
-  styles: [
-  ]
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit {
+  @ViewChild('zoomMap') mapDiv!: ElementRef;
+  public map!: mapboxgl.Map;
+  public zoomLevel: number = 10;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+      center: [-103.38991542336164, 20.65440818024887],
+      container: this.mapDiv.nativeElement,
+      minZoom: 2,
+      maxZoom: 17,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      zoom: this.zoomLevel,
+    });
   }
 
+  zoomOut(): void {
+    this.map.zoomOut();
+    this.zoomLevel = this.map.getZoom();
+  }
+
+  zoomIn(): void {
+    this.map.zoomIn();
+    this.zoomLevel = this.map.getZoom();
+  }
 }
