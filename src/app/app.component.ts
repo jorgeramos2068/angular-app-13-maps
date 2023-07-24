31cce156d40938825ddc68c3c2bdaa07b5ac757e
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-24 14:58:23 -0600

    Establish map center property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'maps-app';
+
+  ngOnInit(): void {
+    (mapboxgl as any).accessToken = environment.mapboxToken;
+  }
 }
